f1af77d3c9e44fe93f97d2dc193431de4ecf11fa
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-11-02 08:59:11 -0300

    custom arrows

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,14 @@ $('.carousel').slick({
   slidesToShow: 1,
   autoplay: true,
   autoplaySpeed: 7000,
+  prevArrow: '<button>oi</button>',
+  nextArrow: '<button>tchau</button>',
+  responsive: [
+    {
+      breakpoint: 576,
+      settings: {
+        arrows: false
+      }
+    }
+  ]
 });
